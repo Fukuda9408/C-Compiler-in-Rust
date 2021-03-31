303234ea1e2d7425f35d9ee15fd9775517b3c5d5
JinseiTsurai <61466779+JinseiTsurai@users.noreply.github.com>
2021-04-01 05:55:18 +0900

    Compile one number

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: ./9cc <code>");
+        process::exit(1);
+    }
+
+    let num: i32 = args[1].parse().unwrap();
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("  mov rax, {}", num);
+}
