bfad6d5b944a9dacf32b0d0c789961da23168403
JinseiTsurai <61466779+JinseiTsurai@users.noreply.github.com>
2021-04-06 20:27:03 +0900

    Moidfy test.sh

--- test.sh
+++ test.sh
@@ -3,12 +3,12 @@ assert() {
     expected="$2"
     input="$1"
 
-    ./target/debug/c99 "$input" > tmp.s
+    ./target/debug/c99 "$input;" > tmp.s
     gcc -o tmp tmp.s
     ./tmp
     actual="$?"
 
-    if [ "$actual" = "$expected" ]; then
+    if [ "$actual;" = "$expected;" ]; then
         echo "$input => $actual"
     else
         echo "#input => $expected expected, but got $actual"
@@ -16,31 +16,31 @@ assert() {
     fi
 }
 
-assert " 1 + 2" 3
-assert " 1 + 3 * 4 + 5" 18
-assert " 1 + +2" 3
-assert "1 - -3 " 4
-assert "1 - -(4 + 5 * 2)" 15
-assert "1 + 5 > 5" 1
-assert "1 + 5 > 7" 0
-assert "1 + 5 < 5" 0
-assert "1 + 5 < 7" 1
-assert "1 + 5 >= 6" 1
-assert "1 + 5 >= 7" 0
-assert "1 + 5 <= 5" 0
-assert "1 + 5 <= 6" 1
+assert " 1 + 2;" 3
+assert " 1 + 3 * 4 + 5;" 18
+assert " 1 + +2;" 3
+assert "1 - -3 ;" 4
+assert "1 - -(4 + 5 * 2);" 15
+assert "1 + 5 > 5;" 1
+assert "1 + 5 > 7;" 0
+assert "1 + 5 < 5;" 0
+assert "1 + 5 < 7;" 1
+assert "1 + 5 >= 6;" 1
+assert "1 + 5 >= 7;" 0
+assert "1 + 5 <= 5;" 0
+assert "1 + 5 <= 6;" 1
 
-assert "1 + 5 == 6" 1
-assert "1 + 5 == 5" 0
-assert "1 + 5 != 6" 0
-assert "1 + 5 != 5" 1
+assert "1 + 5 == 6;" 1
+assert "1 + 5 == 5;" 0
+assert "1 + 5 != 6;" 0
+assert "1 + 5 != 5;" 1
 
-assert "5 > 6 == 5 > 6" 1
-assert "5 > 6 == 5 < 6" 0
-assert "5 > 6 != 5 > 6" 0
-assert "5 > 6 != 5 < 6" 1
+assert "5 > 6 == 5 > 6;" 1
+assert "5 > 6 == 5 < 6;" 0
+assert "5 > 6 != 5 > 6;" 0
+assert "5 > 6 != 5 < 6;" 1
 
-assert "a =1;b =1; a = a+b;" 2
-assert " a =3; b = a; c = b + a;" 6
+assert "a =1;b =1; a = a+b;;" 2
+assert ;" a =3; b = a; c = b + a;;" 6
 
 echo OK
